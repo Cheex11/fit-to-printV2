d0535d3dcafc43558e3b9e1a316f8371b30b5d06
Cody Wilson <Cheex11@gmail.com>
2016-08-17 21:31:38 -0700

    added thumbnails and started adjusting gallery background.

--- js/main.js
+++ js/main.js
@@ -10,9 +10,33 @@ $( document ).ready(function() {
 	  handler: function(direction) {
 	    $('.wp2').addClass('animated fadeInUp')
 	  },
-	    offset: '60%'
+	    offset: '45%'
 	  })
-
+	$('.wp3').waypoint(function() {
+		$('.wp3').addClass('animated fadeInLeft');
+	}, {
+		offset: '55%'
+	});
+	$('.wp4').waypoint(function() {
+		$('.wp4').addClass('animated fadeInRight');
+	}, {
+		offset: '55%'
+	});
+	$('.wp5').waypoint(function() {
+		$('.wp5').addClass('animated fadeInLeft');
+	}, {
+		offset: '55%'
+	});
+	$('.wp6').waypoint(function() {
+		$('.wp6').addClass('animated fadeInRight');
+	}, {
+		offset: '55%'
+	});
+	$('.wp7').waypoint(function() {
+		$('.wp7').addClass('animated fadeInUp');
+	}, {
+		offset: '70%'
+	});
 });
 
 
